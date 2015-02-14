9589a5d511cb3ad4317eb4a85a9e53506aa15d8e
JohnMazua <johnmazua@unitec.edu>
2015-02-14 16:26:54 -0600

    Update MiClase.java

--- src/MiClase.java
+++ src/MiClase.java
@@ -3,18 +3,18 @@ public class MiClase {
 	public int x;
 	MiClase(int x)
 	{
-		this.x = x;
+		this.x = x; // obtiene la variable x
 	}
 	
 	//devuelve "x" multiplicado por dos
 	int getXPor2()
 	{
-		return (x * 2);
+		return (x * 2); //multiplica x por 2
 	}
 	
 	//devuelve la suma de "x" y "y"
 	int getXMasY(int y)
 	{
-		return (x + y) ;
+		return (x + y) ; // suma x mas y
 	}
 }
